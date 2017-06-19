010f92fd4abf7b75ee0fc4509e7e2e2813fb9d8b
Florian Eck <florian.eck@el-digital.de>
2017-06-19 23:18:44 +0200

    inital

---
+++ README.md
@@ -0,0 +1,17 @@
+# BetterButgetsheets for EasyRedmine
+
+
+## Install
+
+
+## TODO
+
+
+
+## Info
+
+This Plugin was created by Florian Eck ([EL Digital Solutions](http://www.el-digital.de)) for [akquinet finance & controlling GmbH](http://www.akquinet.de/).
+
+It is licensed under GNU GENERAL PUBLIC LICENSE.
+
+This Plugin only works with easy redmine
---
+++ init.rb
@@ -0,0 +1,14 @@
+Redmine::Plugin.register :easyredmine_better_budgetsheets do
+  name 'BetterBudgetsheets for EasyRedmine'
+  author 'Florian Eck for akquinet'
+  description 'Improve handling and output of easyredmine budgetsheets'
+  version '1.0'
+
+end
+
+require 'easyredmine_better_budgetsheets'
+
+
+Rails.application.config.after_initialize do
+
+end
