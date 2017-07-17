9fa5d4bf6aaea3aace6895a90c6f03b175d124a6
Florian Eck <florian.eck@el-digital.de>
2017-07-17 12:55:21 +0200

    added footer example file

--- app/controllers/better_budgetsheets_factura_controller.rb
+++ app/controllers/better_budgetsheets_factura_controller.rb
@@ -11,16 +11,14 @@ class BetterBudgetsheetsFacturaController < ApplicationController
 
   def print
     load_data
-    header_file     = File.expand_path("../../../public/header.html", __FILE__)
-    header_content  = (File.open(header_file).read rescue nil)
-
+    
     render  pdf: "#{Time.now.to_date} - #{@query_name} - #{@time_entry_groups.project_names.join(' - ')}".gsub(/\\\/\:\*\?\"\<\>\|/, '_'),
             template: "/better_budgetsheets_factura/sheet",
             layout: "pdf",
             orientation: 'Landscape',
-            footer: { right: '[page] / [topage]',  left: "#{@query_name} #{Time.now.to_date.to_de}" },
-            header: { content: header_content  },
-            margin:  {   top: 20, bottom: 20 }
+            footer: pdf_footer_options,
+            header: pdf_header_options,
+            margin:  {  top: 20, bottom: 20 }
   end
 
   private
@@ -38,6 +36,23 @@ class BetterBudgetsheetsFacturaController < ApplicationController
 
     @time_entry_groups = BetterBudgetsheets::TimeEntryGroupingService.new(@time_entries, columns: @column_names, groups: @group_names)
     @time_entry_groups.load_root_set
+  end
+  
+  def pdf_header_options
+    header_file     = File.expand_path("../../../public/header.html", __FILE__)
+    header_content  = (File.open(header_file).read rescue nil)
+    { content: header_content  }
+  end
+  
+  def pdf_footer_options
+    footer_file     = File.expand_path("../../../public/header.html", __FILE__)
+    footer_content  = (File.open(header_file).read rescue nil)
+    
+    if footer_content
+      {content: footer_content }
+    else  
+      { right: '[page] / [topage]',  left: "#{@query_name} #{Time.now.to_date.to_de}" }
+    end  
   end
 
 end
