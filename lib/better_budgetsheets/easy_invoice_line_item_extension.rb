8625132b5707c1085d88762d75b389a7428b1f9d
Florian Eck <it-support@friends-systems.de>
2019-01-20 15:48:55 +0100

    fix time entry id settings

--- lib/better_budgetsheets/easy_invoice_line_item_extension.rb
+++ lib/better_budgetsheets/easy_invoice_line_item_extension.rb
@@ -4,11 +4,11 @@ module BetterBudgetsheets::EasyInvoiceLineItemExtension
   
   included do
     after_save :store_line_item_time_entries
+    has_many :easy_invoice_line_items_time_entries, dependent: :destroy
   end
   
   def time_entry_ids
-    binding.pry
-    @time_entry_ids || EasyInvoiceLineItemsTimeEntry.where(easy_invoice_line_item_id: self.id).pluck(:time_entry_ids)
+    @time_entry_ids ||= EasyInvoiceLineItemsTimeEntry.where(easy_invoice_line_item_id: self.id).pluck(:time_entry_id)
   end
   
   def time_entry_ids=(val)
@@ -17,8 +17,8 @@ module BetterBudgetsheets::EasyInvoiceLineItemExtension
   
   def store_line_item_time_entries
     # cleanup existing entries
-    EasyInvoiceLineItemsTimeEntry.where(easy_invoice_id: self.easy_invoice_id, easy_invoice_line_item_id: self.id).delete_all
-    @time_entry_ids.each do |i|
+    easy_invoice_line_items_time_entries.delete_all
+    time_entry_ids.each do |i|
       EasyInvoiceLineItemsTimeEntry.create(easy_invoice_id: self.easy_invoice_id, easy_invoice_line_item_id: self.id, time_entry_id: i)
     end
   end
