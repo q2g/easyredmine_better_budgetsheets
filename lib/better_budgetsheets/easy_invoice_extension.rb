044ed21b4b2fc009d9df160cdaa7026524e5577b
Florian Eck <it-support@friends-systems.de>
2019-01-20 16:03:33 +0100

    small fix, prep for saving factura

--- lib/better_budgetsheets/easy_invoice_extension.rb
+++ lib/better_budgetsheets/easy_invoice_extension.rb
@@ -6,6 +6,23 @@ module BetterBudgetsheets::EasyInvoiceExtension
     attr_accessor :easy_line_item_time_entry_settings
     has_many :easy_invoice_line_item_time_entries
     belongs_to :easy_budget_sheet_query
+    
+    safe_attributes *%w{performance_from performance_to easy_budget_sheet_query_id}
+  end
+  
+  def query_params_for_factura_sheet
+    url_params = {}
+    url_params[:query_name] = easy_budget_sheet_query.name
+    url_params[:columns]    = easy_budget_sheet_query.column_names
+    url_params[:query_name] = easy_budget_sheet_query.name
+    url_params[:columns]    = easy_budget_sheet_query.column_names
+    url_params[:sort]       = easy_budget_sheet_query.try(:sort_criteria)
+    
+    return url_params
+  end
+  
+  def load_and_assign_factura_sheet_file
+    # TODO
   end
   
 end
